10b13d345a111de116d6a969bbd6b8fc10835e02
Sandeep kumar saket <sandeepks@shriffle.com>
2026-01-19 19:15:26 +0530

    added extra logic in worker file.

--- app/worker/end_date_reminder_mail_worker.rb
+++ app/worker/end_date_reminder_mail_worker.rb
@@ -3,9 +3,13 @@ class EndDateReminderMailWorker
 
   def perform(*args)
     projects = Project.where(end_date: Date.tomorrow)
-    projects.each do |project|
-      project.developers.each do |developer|
-        RemindEndDateMailer.remind_end_date_email(developer, project).deliver_now
+    if projects.present?
+      projects.each do |project|
+        if project.developers.present?
+          project.developers.each do |developer|
+            RemindEndDateMailer.remind_end_date_email(developer, project).deliver_now
+          end
+        end
       end
     end
   end
