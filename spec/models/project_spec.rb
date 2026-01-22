f28dbee4f4dcc529585871f45e4f0c4105beb62f
Sandeep kumar saket <sandeepks@shriffle.com>
2026-01-22 11:49:19 +0530

    some more test cases

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,23 +2,35 @@ require './spec/rails_helper'
 
 RSpec.describe Project, type: :model do
 
-  describe "status enum" do
-    it "defines the correct integer mapping for statuses" do
-      expect(Project.statuses[:pending]).to eql(0)
-      expect(Project.statuses[:in_progress]).to eql(1)
-      expect(Project.statuses[:completed]).to eql(2)
-    end
+  let(:project) {Project.create(name: "Hello RUby")}
 
-    it "allows setting and reading the status using symbols" do
-      project = Project.new(status: :pending)
-      expect(project.status).to eq("pending")
-      expect(project[:status]).to eq("pending")
+  describe "status enum" do
+    it do
+      should define_enum_for(:status).
+      with_values(pending:0, in_progress:1, completed:2).backed_by_column_of_type(:integer)
     end
   end
-
 
   describe "associations" do
-   it {should belong_to(:manager)}
+    it "must belong to the manager" do
+      should belong_to(:manager)
+    end
 
+    it "must have many tasks" do
+      should have_many(:tasks)
+    end
+
+    it "returns an empty collection when no associated tasks are present" do
+      expect(project.tasks).to be_empty
+    end
+
+    it "checks for the associated tasks to be present" do
+      task1 = project.tasks.new(name: "task1")
+      task2 = project.tasks.new(name: "task2")
+
+      expect(project.tasks).to be_present
+    end
   end
+
+
 end
